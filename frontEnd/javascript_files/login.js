98ff013a62c388530e1c9d8662122491ba4101d7
adane-desta <addonegn@gmail.com>
2026-03-29 09:50:52 +0300

    build

--- frontEnd/javascript_files/login.js
+++ frontEnd/javascript_files/login.js
@@ -76,7 +76,7 @@ loginButton.addEventListener('click', async () => {
     const userType = document.querySelector('input[name="user-type"]:checked').value;
     
     try {
-        const response = await fetch(`http://10.140.160.181:5700/api/login?email=${email}&password=${password}&userType=${userType}`);
+        const response = await fetch(`http://10.140.177.176:5700/api/login?email=${email}&password=${password}&userType=${userType}`);
 
         if (!response.ok) {
             const errorData = await response.json();
